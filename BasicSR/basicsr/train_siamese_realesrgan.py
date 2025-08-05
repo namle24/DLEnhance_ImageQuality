e99f0df96634b54c91c010d2db6f18bb8dfc7949
namle24 <nma2409.na@gmail.com>
2025-08-05 11:56:52 +0700

    stress

--- BasicSR/basicsr/train_siamese_realesrgan.py
+++ BasicSR/basicsr/train_siamese_realesrgan.py
@@ -4,15 +4,13 @@ import torch.nn as nn
 from torch.utils.data import DataLoader
 from basicsr.data import build_dataloader
 from basicsr.models import build_model
-from basicsr.train import parse_options  # Updated import for BasicSR 1.4.2
-from basicsr.utils import create_logger, logging
+from basicsr.train import parse_options
+from basicsr.utils.logger import create_logger  # Updated import for BasicSR 1.4.2
 
 def main():
-    # Parse configuration
-    opt = parse_options(is_train=True)  # Use parse_options; opt_path passed via command line
+    opt = parse_options(is_train=True)
     logger, tb_logger = create_logger(opt, True)
     
-    # Initialize teacher and student models
     opt_teacher = opt.copy()
     opt_student = opt.copy()
     opt_teacher['path']['pretrain_network_g'] = opt.get('path_teacher', None)
@@ -21,29 +19,23 @@ def main():
     teacher_model = build_model(opt_teacher)
     student_model = build_model(opt_student)
     
-    # Move models to device
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     teacher_model.to(device)
     student_model.to(device)
     
-    # Build data loaders for datasets A and B
     opt_teacher['dataset']['train']['name'] = 'DatasetA'
     opt_student['dataset']['train']['name'] = 'DatasetB'
     train_loader_a = build_dataloader(opt_teacher['dataset']['train'], opt_teacher)
     train_loader_b = build_dataloader(opt_student['dataset']['train'], opt_student)
     
-    # Initialize iterators
     iter_a = iter(train_loader_a)
     iter_b = iter(train_loader_b)
     
-    # Initialize distillation loss
     distillation_loss_fn = nn.L1Loss()
     distillation_weight = opt['train'].get('distillation_weight', 0.1)
     
-    # Training loop
     total_iter = opt['train']['total_iter']
     for current_iter in range(total_iter):
-        # Get data from both loaders
         try:
             data_a = next(iter_a)
         except StopIteration:
@@ -56,26 +48,22 @@ def main():
             iter_b = iter(train_loader_b)
             data_b = next(iter_b)
         
-        # Train teacher on dataset A
         teacher_model.feed_data(data_a)
         teacher_model.optimize_parameters(current_iter)
         teacher_loss = teacher_model.log_dict
         teacher_output = teacher_model.output.detach()
         
-        # Train student on dataset B
         student_model.feed_data(data_b)
         student_model.optimize_parameters(current_iter)
         student_loss = student_model.log_dict
         student_output = student_model.output
         
-        # Distillation loss
         if opt['train'].get('distillation_loss', False):
             distillation_loss = distillation_loss_fn(student_output, teacher_output) * distillation_weight
             student_model.optimizer_g.zero_grad()
             distillation_loss.backward()
             student_model.optimizer_g.step()
         
-        # Siamese feature alignment loss
         if opt['train'].get('siamese_loss', False):
             teacher_features = teacher_model.net_g.module.get_intermediate_features(data_a['lq'])
             student_features = student_model.net_g.module.get_intermediate_features(data_b['lq'])
@@ -84,7 +72,6 @@ def main():
             siamese_loss.backward()
             student_model.optimizer_g.step()
         
-        # Logging
         if current_iter % opt['logger']['print_freq'] == 0:
             log_message = f"Iter {current_iter}: Teacher Loss: {teacher_loss}, Student Loss: {student_loss}"
             if opt['train'].get('distillation_loss', False):
@@ -100,7 +87,6 @@ def main():
                 if opt['train'].get('siamese_loss', False):
                     tb_logger.add_scalar('loss/siamese', siamese_loss.item(), current_iter)
         
-        # Save models
         if current_iter % opt['logger']['save_checkpoint_freq'] == 0:
             teacher_model.save(current_iter, 'teacher')
             student_model.save(current_iter, 'student')
