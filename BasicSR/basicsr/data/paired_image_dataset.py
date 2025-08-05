2ae69cd45f10dbd0a1098e2e47a7fef2c2138e61
namle24 <nma2409.na@gmail.com>
2025-08-05 10:30:47 +0700

    siamese check before train

--- BasicSR/basicsr/data/paired_image_dataset.py
+++ BasicSR/basicsr/data/paired_image_dataset.py
@@ -1,7 +1,6 @@
 from torch.utils import data as data
 from torchvision.transforms.functional import normalize
 
-from basicsr.data.data_util import paired_paths_from_folder
 from basicsr.data.transforms import augment, paired_random_crop
 from basicsr.utils import FileClient, bgr2ycbcr, imfrombytes, img2tensor
 from basicsr.utils.registry import DATASET_REGISTRY
@@ -9,58 +8,68 @@ from basicsr.utils.registry import DATASET_REGISTRY
 
 @DATASET_REGISTRY.register()
 class PairedImageDataset(data.Dataset):
-    """Modified PairedImageDataset to support Siamese training with LQ A and LQ B images."""
+    """Paired Siamese dataset for image restoration.
+
+    Reads LQ_A, LQ_B, and GT image triplets.
+
+    Required keys in opt:
+        - dataroot_gt
+        - dataroot_lq_a
+        - dataroot_lq_b
+        - io_backend
+        - phase: train/val
+    """
 
     def __init__(self, opt):
-        super(PairedImageDataset, self).__init__()
+        super().__init__()
         self.opt = opt
         self.file_client = None
         self.io_backend_opt = opt['io_backend']
         self.mean = opt.get('mean', None)
         self.std = opt.get('std', None)
-        self.scale = opt.get('scale', 4)
-        self.phase = opt.get('phase', 'train')
 
-        # Support siamese inputs
         self.gt_folder = opt['dataroot_gt']
         self.lq_a_folder = opt['dataroot_lq_a']
         self.lq_b_folder = opt['dataroot_lq_b']
 
-        self.paths_gt = sorted(paired_paths_from_folder([self.gt_folder], ['gt'], '{}'))
-        self.paths_lq_a = sorted(paired_paths_from_folder([self.lq_a_folder], ['lq_a'], '{}'))
-        self.paths_lq_b = sorted(paired_paths_from_folder([self.lq_b_folder], ['lq_b'], '{}'))
+        gt_paths = sorted(os.listdir(self.gt_folder))
+        lq_a_paths = sorted(os.listdir(self.lq_a_folder))
+        lq_b_paths = sorted(os.listdir(self.lq_b_folder))
 
-        assert len(self.paths_gt) == len(self.paths_lq_a) == len(self.paths_lq_b), 'Dataset size mismatch!'
+        assert len(gt_paths) == len(lq_a_paths) == len(lq_b_paths), 'Số lượng ảnh không khớp!'
+
+        # tạo danh sách dict kiểu giống code gốc
+        self.paths = []
+        for gt, lqa, lqb in zip(gt_paths, lq_a_paths, lq_b_paths):
+            self.paths.append({
+                'gt_path': os.path.join(self.gt_folder, gt),
+                'lq_a_path': os.path.join(self.lq_a_folder, lqa),
+                'lq_b_path': os.path.join(self.lq_b_folder, lqb)
+            })
 
     def __getitem__(self, index):
         if self.file_client is None:
             self.file_client = FileClient(self.io_backend_opt.pop('type'), **self.io_backend_opt)
 
-        # Load image paths
-        gt_path = self.paths_gt[index]['gt_path']
-        lq_a_path = self.paths_lq_a[index]['lq_a_path']
-        lq_b_path = self.paths_lq_b[index]['lq_b_path']
+        scale = self.opt['scale']
+        gt_size = self.opt.get('gt_size', 256)
 
-        # Read images
-        img_gt = imfrombytes(self.file_client.get(gt_path, 'gt'), float32=True)
-        img_lq_a = imfrombytes(self.file_client.get(lq_a_path, 'lq_a'), float32=True)
-        img_lq_b = imfrombytes(self.file_client.get(lq_b_path, 'lq_b'), float32=True)
+        paths = self.paths[index]
+        gt_path, lq_a_path, lq_b_path = paths['gt_path'], paths['lq_a_path'], paths['lq_b_path']
 
-        # Random crop
-        if self.phase == 'train':
-            gt_size = self.opt['gt_size']
-            img_gt, img_lq_a = paired_random_crop(img_gt, img_lq_a, gt_size, self.scale, gt_path)
-            _, img_lq_b = paired_random_crop(img_gt, img_lq_b, gt_size, self.scale, gt_path)
+        img_gt = imfrombytes(self.file_client.get(gt_path), float32=True)
+        img_lq_a = imfrombytes(self.file_client.get(lq_a_path), float32=True)
+        img_lq_b = imfrombytes(self.file_client.get(lq_b_path), float32=True)
 
-            # Augment
-            img_gt, img_lq_a, img_lq_b = augment([img_gt, img_lq_a, img_lq_b],
-                                                 self.opt.get('use_hflip', True),
-                                                 self.opt.get('use_rot', True))
+        if self.opt['phase'] == 'train':
+            img_gt, img_lq_a = paired_random_crop(img_gt, img_lq_a, gt_size, scale, gt_path)
+            _, img_lq_b = paired_random_crop(img_gt, img_lq_b, gt_size, scale, gt_path)
+            img_gt, img_lq_a = augment([img_gt, img_lq_a], self.opt.get('use_hflip', True), self.opt.get('use_rot', True))
+            _, img_lq_b = augment([img_gt, img_lq_b], self.opt.get('use_hflip', True), self.opt.get('use_rot', True))
 
-        # BGR to RGB, HWC to CHW, to tensor
+        # to tensor
         img_gt, img_lq_a, img_lq_b = img2tensor([img_gt, img_lq_a, img_lq_b], bgr2rgb=True, float32=True)
 
-        # Normalize if needed
         if self.mean is not None or self.std is not None:
             normalize(img_gt, self.mean, self.std, inplace=True)
             normalize(img_lq_a, self.mean, self.std, inplace=True)
@@ -70,8 +79,10 @@ class PairedImageDataset(data.Dataset):
             'gt': img_gt,
             'lq_a': img_lq_a,
             'lq_b': img_lq_b,
-            'gt_path': gt_path
+            'gt_path': gt_path,
+            'lq_a_path': lq_a_path,
+            'lq_b_path': lq_b_path
         }
 
     def __len__(self):
-        return len(self.paths_gt)
+        return len(self.paths)
