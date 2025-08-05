5427d2435cccd06cb30a0394f38960033b7ac66e
namle24 <nma2409.na@gmail.com>
2025-08-05 10:28:59 +0700

    siamese check before train

--- BasicSR/basicsr/data/paired_image_dataset.py
+++ BasicSR/basicsr/data/paired_image_dataset.py
@@ -1,7 +1,7 @@
 from torch.utils import data as data
 from torchvision.transforms.functional import normalize
 
-from basicsr.data.data_util import paired_paths_from_folder, paired_paths_from_lmdb, paired_paths_from_meta_info_file
+from basicsr.data.data_util import paired_paths_from_folder
 from basicsr.data.transforms import augment, paired_random_crop
 from basicsr.utils import FileClient, bgr2ycbcr, imfrombytes, img2tensor
 from basicsr.utils.registry import DATASET_REGISTRY
@@ -9,98 +9,69 @@ from basicsr.utils.registry import DATASET_REGISTRY
 
 @DATASET_REGISTRY.register()
 class PairedImageDataset(data.Dataset):
-    """Paired image dataset for image restoration.
-
-    Read LQ (Low Quality, e.g. LR (Low Resolution), blurry, noisy, etc) and GT image pairs.
-
-    There are three modes:
-
-    1. **lmdb**: Use lmdb files. If opt['io_backend'] == lmdb.
-    2. **meta_info_file**: Use meta information file to generate paths. \
-        If opt['io_backend'] != lmdb and opt['meta_info_file'] is not None.
-    3. **folder**: Scan folders to generate paths. The rest.
-
-    Args:
-        opt (dict): Config for train datasets. It contains the following keys:
-        dataroot_gt (str): Data root path for gt.
-        dataroot_lq (str): Data root path for lq.
-        meta_info_file (str): Path for meta information file.
-        io_backend (dict): IO backend type and other kwarg.
-        filename_tmpl (str): Template for each filename. Note that the template excludes the file extension.
-            Default: '{}'.
-        gt_size (int): Cropped patched size for gt patches.
-        use_hflip (bool): Use horizontal flips.
-        use_rot (bool): Use rotation (use vertical flip and transposing h and w for implementation).
-        scale (bool): Scale, which will be added automatically.
-        phase (str): 'train' or 'val'.
-    """
+    """Modified PairedImageDataset to support Siamese training with LQ A and LQ B images."""
 
     def __init__(self, opt):
         super(PairedImageDataset, self).__init__()
         self.opt = opt
-        # file client (io backend)
         self.file_client = None
         self.io_backend_opt = opt['io_backend']
-        self.mean = opt['mean'] if 'mean' in opt else None
-        self.std = opt['std'] if 'std' in opt else None
+        self.mean = opt.get('mean', None)
+        self.std = opt.get('std', None)
+        self.scale = opt.get('scale', 4)
+        self.phase = opt.get('phase', 'train')
 
-        self.gt_folder, self.lq_folder = opt['dataroot_gt'], opt['dataroot_lq']
-        if 'filename_tmpl' in opt:
-            self.filename_tmpl = opt['filename_tmpl']
-        else:
-            self.filename_tmpl = '{}'
+        # Support siamese inputs
+        self.gt_folder = opt['dataroot_gt']
+        self.lq_a_folder = opt['dataroot_lq_a']
+        self.lq_b_folder = opt['dataroot_lq_b']
 
-        if self.io_backend_opt['type'] == 'lmdb':
-            self.io_backend_opt['db_paths'] = [self.lq_folder, self.gt_folder]
-            self.io_backend_opt['client_keys'] = ['lq', 'gt']
-            self.paths = paired_paths_from_lmdb([self.lq_folder, self.gt_folder], ['lq', 'gt'])
-        elif 'meta_info_file' in self.opt and self.opt['meta_info_file'] is not None:
-            self.paths = paired_paths_from_meta_info_file([self.lq_folder, self.gt_folder], ['lq', 'gt'],
-                                                          self.opt['meta_info_file'], self.filename_tmpl)
-        else:
-            self.paths = paired_paths_from_folder([self.lq_folder, self.gt_folder], ['lq', 'gt'], self.filename_tmpl)
+        self.paths_gt = sorted(paired_paths_from_folder([self.gt_folder], ['gt'], '{}'))
+        self.paths_lq_a = sorted(paired_paths_from_folder([self.lq_a_folder], ['lq_a'], '{}'))
+        self.paths_lq_b = sorted(paired_paths_from_folder([self.lq_b_folder], ['lq_b'], '{}'))
+
+        assert len(self.paths_gt) == len(self.paths_lq_a) == len(self.paths_lq_b), 'Dataset size mismatch!'
 
     def __getitem__(self, index):
         if self.file_client is None:
             self.file_client = FileClient(self.io_backend_opt.pop('type'), **self.io_backend_opt)
 
-        scale = self.opt['scale']
+        # Load image paths
+        gt_path = self.paths_gt[index]['gt_path']
+        lq_a_path = self.paths_lq_a[index]['lq_a_path']
+        lq_b_path = self.paths_lq_b[index]['lq_b_path']
 
-        # Load gt and lq images. Dimension order: HWC; channel order: BGR;
-        # image range: [0, 1], float32.
-        gt_path = self.paths[index]['gt_path']
-        img_bytes = self.file_client.get(gt_path, 'gt')
-        img_gt = imfrombytes(img_bytes, float32=True)
-        lq_path = self.paths[index]['lq_path']
-        img_bytes = self.file_client.get(lq_path, 'lq')
-        img_lq = imfrombytes(img_bytes, float32=True)
+        # Read images
+        img_gt = imfrombytes(self.file_client.get(gt_path, 'gt'), float32=True)
+        img_lq_a = imfrombytes(self.file_client.get(lq_a_path, 'lq_a'), float32=True)
+        img_lq_b = imfrombytes(self.file_client.get(lq_b_path, 'lq_b'), float32=True)
 
-        # augmentation for training
-        if self.opt['phase'] == 'train':
+        # Random crop
+        if self.phase == 'train':
             gt_size = self.opt['gt_size']
-            # random crop
-            img_gt, img_lq = paired_random_crop(img_gt, img_lq, gt_size, scale, gt_path)
-            # flip, rotation
-            img_gt, img_lq = augment([img_gt, img_lq], self.opt['use_hflip'], self.opt['use_rot'])
+            img_gt, img_lq_a = paired_random_crop(img_gt, img_lq_a, gt_size, self.scale, gt_path)
+            _, img_lq_b = paired_random_crop(img_gt, img_lq_b, gt_size, self.scale, gt_path)
 
-        # color space transform
-        if 'color' in self.opt and self.opt['color'] == 'y':
-            img_gt = bgr2ycbcr(img_gt, y_only=True)[..., None]
-            img_lq = bgr2ycbcr(img_lq, y_only=True)[..., None]
+            # Augment
+            img_gt, img_lq_a, img_lq_b = augment([img_gt, img_lq_a, img_lq_b],
+                                                 self.opt.get('use_hflip', True),
+                                                 self.opt.get('use_rot', True))
 
-        # crop the unmatched GT images during validation or testing, especially for SR benchmark datasets
-        # TODO: It is better to update the datasets, rather than force to crop
-        if self.opt['phase'] != 'train':
-            img_gt = img_gt[0:img_lq.shape[0] * scale, 0:img_lq.shape[1] * scale, :]
+        # BGR to RGB, HWC to CHW, to tensor
+        img_gt, img_lq_a, img_lq_b = img2tensor([img_gt, img_lq_a, img_lq_b], bgr2rgb=True, float32=True)
 
-        # BGR to RGB, HWC to CHW, numpy to tensor
-        img_gt, img_lq = img2tensor([img_gt, img_lq], bgr2rgb=True, float32=True)
-        # normalize
+        # Normalize if needed
         if self.mean is not None or self.std is not None:
-            normalize(img_lq, self.mean, self.std, inplace=True)
             normalize(img_gt, self.mean, self.std, inplace=True)
+            normalize(img_lq_a, self.mean, self.std, inplace=True)
+            normalize(img_lq_b, self.mean, self.std, inplace=True)
 
-        return {'lq': img_lq, 'gt': img_gt, 'lq_path': lq_path, 'gt_path': gt_path}
+        return {
+            'gt': img_gt,
+            'lq_a': img_lq_a,
+            'lq_b': img_lq_b,
+            'gt_path': gt_path
+        }
 
     def __len__(self):
-        return len(self.paths)
+        return len(self.paths_gt)
