c872702de7e991749a2e3a4b8dbc45c27b58aba7
namle24 <nma2409.na@gmail.com>
2025-08-06 13:12:29 +0700

    siamese

--- Real-ESRGAN/basicsr/archs/rrdbnet_featout_arch.py
+++ Real-ESRGAN/basicsr/archs/rrdbnet_featout_arch.py
@@ -7,19 +7,20 @@ from basicsr.utils.registry import ARCH_REGISTRY
 @ARCH_REGISTRY.register()
 class RRDBNetFeatOut(RRDBNet):
     """RRDBNet mở rộng để xuất đặc trưng trung gian (feature maps) cho Knowledge Distillation."""
-    def __init__(self, **kwargs):
+
+    def __init__(self, return_intermediate=False, **kwargs):
+        self.return_intermediate = return_intermediate
         super().__init__(**kwargs)
-        self.return_intermediate = kwargs.get('return_intermediate', False)
 
     def forward(self, x):
         if self.return_intermediate:
-            # Lấy feature đầu ra trước khi lên pixel
+            # Lấy feature đầu ra trước khi pixel shuffle
             fea = self.conv_first(x)
             trunk = self.rrdb_trunk(fea)
             fea = fea + self.trunk_conv(trunk)
             out = self.upconv1(fea)
             out = self.upconv2(out)
             out = self.conv_hr(self.lrelu(self.conv_last(out)))
-            return out, fea  # trả về SR + đặc trưng trung gian
+            return out, fea  # output + intermediate features
         else:
             return super().forward(x)
