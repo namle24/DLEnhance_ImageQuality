331bb6ae410d8ced138a37aa07aa48574aa7d28c
namle24 <nma2409.na@gmail.com>
2025-08-08 15:03:16 +0700

    change code to test again siamese

--- Real-ESRGAN/basicsr/data/paired_siamese_image_dataset.py
+++ Real-ESRGAN/basicsr/data/paired_siamese_image_dataset.py
@@ -86,20 +86,16 @@ class PairedSiameseImageDataset(BaseDataset):
             expected_h_lq = h_gt // self.scale
             expected_w_lq = w_gt // self.scale
 
-            # Resize LQ images to expected size if needed (using INTER_AREA for downscaling)
-            if img_lq_a.shape[:2] != (expected_h_lq, expected_w_lq):
-                img_lq_a = cv2.resize(img_lq_a, (expected_w_lq, expected_h_lq), interpolation=cv2.INTER_AREA)
-            if img_lq_b.shape[:2] != (expected_h_lq, expected_w_lq):
-                img_lq_b = cv2.resize(img_lq_b, (expected_w_lq, expected_h_lq), interpolation=cv2.INTER_AREA)
+            # Resize LQ images to expected size
+            img_lq_a = cv2.resize(img_lq_a, (expected_w_lq, expected_h_lq), interpolation=cv2.INTER_AREA)
+            img_lq_b = cv2.resize(img_lq_b, (expected_w_lq, expected_h_lq), interpolation=cv2.INTER_AREA)
 
-            # For training, crop patches
+            # For training, crop fixed-size patches
             if self.phase == 'train' and self.gt_size is not None:
-                # Calculate LQ crop size
                 lq_crop_size = self.gt_size // self.scale
                 
-                # Ensure LQ images are large enough
+                # Pad images if they're too small
                 if img_lq_a.shape[0] < lq_crop_size or img_lq_a.shape[1] < lq_crop_size:
-                    # If image is too small, first pad then crop
                     pad_h = max(0, lq_crop_size - img_lq_a.shape[0])
                     pad_w = max(0, lq_crop_size - img_lq_a.shape[1])
                     
@@ -107,35 +103,29 @@ class PairedSiameseImageDataset(BaseDataset):
                     img_lq_b = cv2.copyMakeBorder(img_lq_b, 0, pad_h, 0, pad_w, cv2.BORDER_REFLECT)
                     img_gt = cv2.copyMakeBorder(img_gt, 0, pad_h * self.scale, 0, pad_w * self.scale, cv2.BORDER_REFLECT)
 
-                # Random crop coordinates for LQ
+                # Random crop
                 top = random.randint(0, img_lq_a.shape[0] - lq_crop_size)
                 left = random.randint(0, img_lq_a.shape[1] - lq_crop_size)
                 
-                # Crop LQ images
                 img_lq_a = img_lq_a[top:top + lq_crop_size, left:left + lq_crop_size, :]
                 img_lq_b = img_lq_b[top:top + lq_crop_size, left:left + lq_crop_size, :]
-                
-                # Calculate corresponding GT crop coordinates and crop
-                top_gt, left_gt = top * self.scale, left * self.scale
-                img_gt = img_gt[top_gt:top_gt + self.gt_size, left_gt:left_gt + self.gt_size, :]
+                img_gt = img_gt[top*self.scale:(top + lq_crop_size)*self.scale, 
+                            left*self.scale:(left + lq_crop_size)*self.scale, :]
 
-            # Data augmentation (sync for all images)
+            # Data augmentation
             img_gt, img_lq_a, img_lq_b = augment([img_gt, img_lq_a, img_lq_b], self.use_flip, self.use_rot)
 
-            # Convert to tensor
-            img_gt = img2tensor(img_gt, bgr2rgb=True, float32=True)
-            img_lq_a = img2tensor(img_lq_a, bgr2rgb=True, float32=True)
-            img_lq_b = img2tensor(img_lq_b, bgr2rgb=True, float32=True)
-
-            # Normalize
-            for t in [img_gt, img_lq_a, img_lq_b]:
+            # Convert to tensor and normalize
+            def _process(img):
+                img = img2tensor(img, bgr2rgb=True, float32=True)
                 for c in range(3):
-                    t[c, :, :] = (t[c, :, :] - self.mean[c]) / self.std[c]
+                    img[c] = (img[c] - self.mean[c]) / self.std[c]
+                return img
 
             return {
-                'gt': img_gt,
-                'lq_a': img_lq_a,
-                'lq_b': img_lq_b,
+                'gt': _process(img_gt),
+                'lq_a': _process(img_lq_a),
+                'lq_b': _process(img_lq_b),
                 'gt_path': gt_path,
                 'lq_a_path': lq_a_path,
                 'lq_b_path': lq_b_path
