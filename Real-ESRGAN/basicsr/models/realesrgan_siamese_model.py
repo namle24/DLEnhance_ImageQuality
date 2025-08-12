1080fb8605f3231e663928e8957c9dee107c4313
namle24 <nma2409.na@gmail.com>
2025-08-12 15:27:45 +0700

    Last hope server working well

--- Real-ESRGAN/basicsr/models/realesrgan_siamese_model.py
+++ Real-ESRGAN/basicsr/models/realesrgan_siamese_model.py
@@ -4,57 +4,46 @@ from basicsr.utils.registry import MODEL_REGISTRY
 from realesrgan.models.realesrgan_model import RealESRGANModel
 import os.path as osp
 from basicsr.utils import imwrite, tensor2img
-from torch.cuda.amp import autocast, GradScaler
 
 @MODEL_REGISTRY.register()
 class RealESRGANSiameseModel(RealESRGANModel):
-    def __init__(self, opt):
-        super().__init__(opt)
-        self.scaler = GradScaler(enabled=opt['train'].get('use_amp', False))
-        
     def feed_data(self, data):
-        self.lq_a = data['lq_a'].to(self.device, non_blocking=True)
-        self.lq_b = data['lq_b'].to(self.device, non_blocking=True)
-        self.gt = data['gt'].to(self.device, non_blocking=True)
+        self.lq_a = data['lq_a'].to(self.device)
+        self.lq_b = data['lq_b'].to(self.device)
+        self.gt = data['gt'].to(self.device)
 
     def optimize_parameters(self, current_iter):
-        use_amp = self.opt['train'].get('use_amp', False)
-        
-        with autocast(enabled=use_amp):
-            # Teacher forward
-            with torch.no_grad():
-                self.output_a, self.feat_a = self.net_g(self.lq_a, return_feats=True)
+        # Teacher forward
+        with torch.no_grad():
+            self.output_a, self.feat_a = self.net_g(self.lq_a, return_feats=True)
 
-            # Student forward
-            self.output_b, self.feat_b = self.net_g(self.lq_b, return_feats=True)
+        # Student forward
+        self.output_b, self.feat_b = self.net_g(self.lq_b, return_feats=True)
 
-            # Calculate all losses
-            l_pix = self.cri_pix(self.output_b, self.gt)
-            l_kd_out = torch.nn.functional.l1_loss(self.output_b, self.output_a.detach())
-            l_kd_feat = torch.nn.functional.l1_loss(self.feat_b, self.feat_a.detach())
+        # Pixel loss
+        l_pix = self.cri_pix(self.output_b, self.gt)
 
-            # Perceptual and style losses
-            l_percep, l_style = 0, 0
-            if self.cri_perceptual:
-                l_percep, l_style = self.cri_perceptual(self.output_b, self.gt)
+        # KD output loss
+        l_kd_out = torch.nn.functional.l1_loss(self.output_b, self.output_a.detach())
 
-            # Total loss with weighted components
-            loss = l_pix + \
-                   self.opt['train']['lambda_kd_out'] * l_kd_out + \
-                   self.opt['train']['lambda_kd_feat'] * l_kd_feat + \
-                   l_percep + l_style
+        # KD feature loss
+        l_kd_feat = torch.nn.functional.l1_loss(self.feat_b, self.feat_a.detach())
 
-        # Optimization
-        self.optimizer_g.zero_grad(set_to_none=True)
-        if use_amp:
-            self.scaler.scale(loss).backward()
-            self.scaler.step(self.optimizer_g)
-            self.scaler.update()
-        else:
-            loss.backward()
-            self.optimizer_g.step()
+        # Perceptual loss
+        l_percep, l_style = 0, 0
+        if self.cri_perceptual:
+            l_percep, l_style = self.cri_perceptual(self.output_b, self.gt)
 
-        # Logging
+        # Total
+        loss = l_pix + \
+               self.opt['train']['lambda_kd_out'] * l_kd_out + \
+               self.opt['train']['lambda_kd_feat'] * l_kd_feat + \
+               l_percep + l_style
+
+        self.optimizer_g.zero_grad()
+        loss.backward()
+        self.optimizer_g.step()
+
         self.log_dict = {
             'l_pix': l_pix.item(),
             'l_kd_out': l_kd_out.item(),
