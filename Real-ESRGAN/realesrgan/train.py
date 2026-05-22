0e03a249f495dcfc85230fcae7f83a8aab9c925f
namle24 <nma2409.na@gmail.com>
2026-05-22 18:09:46 +0700

    update degradation gen pipeline

--- Real-ESRGAN/realesrgan/train.py
+++ Real-ESRGAN/realesrgan/train.py
@@ -1,8 +1,11 @@
 import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import sys
-sys.path.insert(0, '/home/namlh/DLEnhance_ImageQuality/Real-ESRGAN')
+
+# Dynamically add Real-ESRGAN root to path (works on any machine)
+_real_esrgan_root = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+if _real_esrgan_root not in sys.path:
+    sys.path.insert(0, _real_esrgan_root)
+
 import os.path as osp
 from basicsr.train import train_pipeline
 
